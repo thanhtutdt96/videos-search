f1c067da69d60d8e569abb6d9abfc610cdd88ca4
Tu Pham <tupham96@MacBook-Pro.local>
2020-07-20 11:58:30 +0700

    Updated components and apis

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div className="ui segment">
+      <div className="ui embed">
+        <iframe src={videoSrc} title={video.snippet.title} />
+      </div>
+      <h4 className="ui header">{video.snippet.title}</h4>
+      <p>{video.snippet.description}</p>
+    </div>
+  );
+};
+
+export default VideoDetail;
